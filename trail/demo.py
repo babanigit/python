341a06b79ce6c7f70db64a6645a9c2b36ff5337f
Aniket Panchal <aniketpanchal07@gmail.com>
2023-11-12 20:43:16 +0530

    new files added

--- trail/demo.py
+++ trail/demo.py
@@ -21,13 +21,14 @@ x = {1 + 2 + 3 + 4 + 5 + 6 +
 print(x)
 result = (
     25 + 100 
-    * 5 -3
+    * 5 - 3
 )
 print("the result is",result,"\n")
 
 x = 12
 y = 6
 trailList = {1,2,3,4,5};
+
 if (x in trailList):
     print (x, "is available in the list ")
 else:
@@ -37,3 +38,14 @@ if (type(x) is int ):
     print("ist an integer and trail lists")
 else:
     print("its not an integer and not a trail lists")
+
+
+# String1 = input("enter the string of your choice : ")
+
+# print(String1[slice(3,5)])
+ 
+number = (1,4,3,8,9,7,6,2,5)
+print(type(number))
+number2 = sorted(number)
+print(number2)
+print(type(number2))
