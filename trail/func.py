77a3686321923402f167085650fcb367d1f6d0f3
aniket panchal <aniketpanchal07@gmail.com>
2023-12-06 23:03:22 +0530

    modified

--- trail/func.py
+++ trail/func.py
@@ -1,24 +1,4 @@
-
-def fun(x):
-    z = x + 3
-
-    def fun2(x):
-
-        z = 3 + x
-        print(z)
-
-    fun2(z)
-
-print("hello")
-
-fun(2)
-
-print("hello again ")
-
-
-def fun3():
-
-    print("hello fun 3")
-
-
-fun3()
+def fun():
+    print("hello")
+    
+fun()
